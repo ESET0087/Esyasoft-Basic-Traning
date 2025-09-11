dadc7612f2e973a014dbb3c255fe37ab39131d62
Dharmesh Kumar <dharmesh.kumar@esyasoft.com>
2025-09-11 16:48:20 +0530

    update assessment 2

--- Basics of MSSQL/Week 2/Assessment 2.sql
+++ Basics of MSSQL/Week 2/Assessment 2.sql
@@ -1,12 +1,16 @@
 -- task 1 - find the top 2 most expensive books ordered by each user
 
-select u.user_id, u.name, b.title, b.price, 
-row_number () over (partition by u.user_id order by b.price desc) as row_price,
-rank () over (partition by u.user_id order by b.price desc) as rank_price,
-dense_rank () over (partition by u.user_id order by b.price desc) as dense_rank_price
-from orders o
-inner join books b on b.product_id = o.product_id
-inner join users u on u.user_id = o.user_id
+select * from (
+	select u.user_id as id, u.name, b.title, b.price, 
+	row_number () over (partition by u.user_id order by b.price desc) as row_price,
+	rank () over (partition by u.user_id order by b.price desc) as rank_price,
+	dense_rank () over (partition by u.user_id order by b.price desc) as dense_rank_price
+	from orders o
+	inner join books b on b.product_id = o.product_id
+	inner join users u on u.user_id = o.user_id
+) as rank_books
+where row_price <= 2
+order by id, row_price;
 
 -- task 2 - find how much each user spent in total and rank them
 
