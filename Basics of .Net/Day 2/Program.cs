98503d1c5d6405f14f338a9b41da41aae453efa6
Dharmesh Kumar <dharmesh.kumar@esyasoft.com>
2025-09-12 13:10:41 +0530

    add comments

--- Basics of .Net/Day 2/Program.cs
+++ Basics of .Net/Day 2/Program.cs
@@ -10,15 +10,18 @@ namespace Assignment2
         { 
             while (true)
             {
+                // displaing options
                 Console.WriteLine("1. Create a new bank account");
                 Console.WriteLine("2. Deposit Money");
                 Console.WriteLine("3. Withdraw Money");
                 Console.WriteLine("4. Display Account Detail");
                 Console.WriteLine("5. Exit \n");
 
+                // asking inputs
                 Console.WriteLine("Enter the option: ");
                 int option_number = Convert.ToInt32(Console.ReadLine());
 
+                // performing options based on input
                 switch (option_number)
                 {
                     case 1:
@@ -45,6 +48,7 @@ namespace Assignment2
             account_number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the account name: ");
             name = Console.ReadLine();
+            // confirmation
             Console.WriteLine("Account Created Successfully! \n");
         }
 
@@ -52,6 +56,7 @@ namespace Assignment2
         {
             Console.WriteLine("Enter the amount: ");
             balance += Convert.ToInt32(Console.ReadLine());
+            // confirmation
             Console.WriteLine("Deposit Successfull!");
             Console.WriteLine($"Available balance: {balance} \n");
         }
@@ -60,14 +65,16 @@ namespace Assignment2
         {
             Console.WriteLine("Enter the amount: ");
             int debit = Convert.ToInt32(Console.ReadLine());
-
+            // is balance is insufficient
             if (debit > balance)
             {
                 Console.WriteLine("Insufficient Balance. \n");
             }
+            // if enough balance is present to withdraw
             else
             {
                 balance -= debit;
+                // confirmation
                 Console.WriteLine("Withdrawl Successfull!");
                 Console.WriteLine($"Available balance: {balance} \n");
             }
@@ -75,6 +82,7 @@ namespace Assignment2
 
         static void DisplayAccountDetail()
         {
+            // displaying details
             Console.WriteLine($"Account Number: {account_number}, Account Name: {name}, Balance: {balance} \n");
         }
     }
